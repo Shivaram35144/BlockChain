c019abbf2d521bf7a8ce00cdb2c654d5deacdb5d
Shivaram Karthikeyan <shivaram@Shivarams-MacBook-Air-2.local>
2024-02-02 21:29:30 +0530

    first commit

---
+++ contracts/todolist.sol
@@ -0,0 +1,11 @@
+// decalre the version of the solidity program we are using 
+
+pragma solidity >=0.5.2 ;
+
+contract todolist {
+    
+    uint taskCount = 0; // state variable -> the state of blockchain changes anytime this variable is changed
+    
+
+}
+
